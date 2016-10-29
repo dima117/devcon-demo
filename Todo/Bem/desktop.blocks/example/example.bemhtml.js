88690fc70bf7fd64bedcd962d3c502c3288fbc94
dima117 <dima117a@gmail.com>
2016-10-29 15:18:51 +0300

    bem infrastructure

---
+++ Todo/Bem/desktop.blocks/example/example.bemhtml.js
@@ -0,0 +1,16 @@
+﻿block('example')(
+    content()([
+        {
+            elem: 'title',
+            content: 'This is an example block'
+        },
+        {
+            elem: 'body',
+            content: 'Lorem ipsum dolor sit amet, mea nisl antiopam id, explicari maiestatis has no.'
+        }
+    ]),
+
+    elem('title').tag()('h1'),
+
+    elem('body').tag()('p')
+);
