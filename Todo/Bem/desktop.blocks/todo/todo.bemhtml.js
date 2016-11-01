9caa86fd4dadd2e0e4fb577e7e61ce74241b559a
Anton Winogradov <winogradovaa@gmail.com>
2016-11-01 11:05:23 +0300

    comments (#7)

--- Todo/Bem/desktop.blocks/todo/todo.bemhtml.js
+++ Todo/Bem/desktop.blocks/todo/todo.bemhtml.js
@@ -1,32 +1,30 @@
 block('todo')(
 
-  tag()('main'),
-
-  js()(true),
+  // js()(true),
 
   content()(function () {
     return [
-      {
-        elem: 'form',
-        content: [
-          {
-            block: 'input',
-            mods: { theme: 'islands', size: 'm' },
-            mix: { block: this.block, elem: 'text' }
-          },
-          '&nbsp;',
-          {
-            block: 'button',
-            mods: { theme: 'islands', view: 'action', size: 'm' },
-            mix: { block: this.block, elem: 'add' },
-            text: 'Добавить'
-          }
-        ]
-      },
-      {
-        block: 'todo-list',
-        items: this.ctx.items
-      }
+      // {
+      //   elem: 'form',
+      //   content: [
+      //     // {
+      //     //   block: 'input',
+      //     //   mods: { theme: 'islands', size: 'm' },
+      //     //   // mix: { block: this.block, elem: 'text' }
+      //     // },
+      //     // '&nbsp;',
+      //     // {
+      //     //   block: 'button',
+      //     //   mods: { theme: 'islands', view: 'action', size: 'm' },
+      //     //   // mix: { block: this.block, elem: 'add' },
+      //     //   text: 'Добавить'
+      //     // }
+      //   ]
+      // },
+      // {
+      //   block: 'todo-list',
+      //   items: this.ctx.items
+      // }
     ];
   }),
 
